b0eadffe9834c0c699e183c4efec09dfaae27de7
Yimmi-urbano <yurbanoantunez@gmail.com>
2024-11-28 21:54:27 -0500

    fix order date

--- models/Order.js
+++ models/Order.js
@@ -13,28 +13,25 @@ const orderSchema = new mongoose.Schema({
     total: { type: Number },
     currency: { type: String },
     orderStatus: { type: String, enum: ['pending', 'shipped', 'delivered', 'cancelled'], default: 'pending' },
-    createdAt: { type: String }
+    createdAt: { type: Date }  // Aquí solo dejamos el tipo de dato como Date
 });
 
+// Middleware para configurar la zona horaria de Lima (UTC-5) y generar orderNumber
 orderSchema.pre('save', async function (next) {
     const order = this;
 
     if (order.isNew) {
         try {
             // Configura la zona horaria a 'America/Lima' (UTC -5)
-            const country = order.clientInfo.country || 'America/Lima';  
-            const now = moment.tz(country); // Obtener la fecha local en la zona horaria correcta
+            const now = moment.tz('America/Lima');  // Obtener la fecha y hora local en Lima
 
-            // Convertir la fecha local a UTC
-            const utcDate = now.utc(); // Convertir a UTC
-
-            // Asignar la fecha UTC al campo createdAt
-            order.createdAt = utcDate.toDate(); // Este es el valor que se almacenará en MongoDB
+            // Asignar la fecha local a createdAt
+            order.createdAt = now.toDate();  // Aquí guardamos la fecha en el campo createdAt en formato UTC
 
             // Generar un timestamp en milisegundos (13 dígitos)
-            const timestamp = utcDate.valueOf(); // Timestamp en milisegundos
-            const ksuidString = await ksuid.random(); 
-            const randomPart = ksuidString.string.slice(0, 7); // Los primeros 7 caracteres del KSUID
+            const timestamp = now.valueOf();  // Timestamp en milisegundos
+            const ksuidString = await ksuid.random();
+            const randomPart = ksuidString.string.slice(0, 7);  // Tomamos los primeros 7 caracteres del KSUID
 
             // Generar el número de orden
             order.orderNumber = `${timestamp}${randomPart}`;
