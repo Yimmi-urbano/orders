c651c7ac09f42f76544a095cf586bf46dbfd9c15
Yimmi-urbano <yurbanoantunez@gmail.com>
2024-11-28 23:04:25 -0500

    fix: date

--- models/Order.js
+++ models/Order.js
@@ -13,33 +13,29 @@ const orderSchema = new mongoose.Schema({
     total: { type: Number },
     currency: { type: String },
     orderStatus: { type: String, enum: ['pending', 'shipped', 'delivered', 'cancelled'], default: 'pending' },
-    createdAt: { type: String }  // Changed to String for ISO 8601 format
+    createdAt: { type: String }
 });
 
 orderSchema.pre('save', async function (next) {
-  const order = this;
+    const order = this;
 
-  if (order.isNew) {
-    try {
-      const now = moment.tz('America/Lima');
+    if (order.isNew) {
+        try {
+            const now = moment.tz('America/Lima');
+            const timestamp = now.valueOf();
+            const ksuidString = await ksuid.random();
+            const randomPart = ksuidString.string.slice(0, 7);
 
-      // Generate order number before saving
-      const timestamp = now.valueOf();
-      const ksuidString = await ksuid.random();
-      const randomPart = ksuidString.string.slice(0, 7);
-      order.orderNumber = `${timestamp}${randomPart}`;
+            order.orderNumber = `${timestamp}${randomPart}`;
+            order.createdAt = now.format('DD-MM-YYYY');
 
-      // Ensure UTC and ISO 8601 format for createdAt
-      order.createdAt = now.utc().toDate().toISOString();
-
-
-      next();
-    } catch (error) {
-      next(error);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();
     }
-  } else {
-    next();
-  }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
